ef878c836979e9c063c0dd44b0e0c60af08a915d
amandabomfim <rm99741@fiap.com.br>
2023-11-09 16:20:29 -0300

    Página Not-found criada.

---
+++ src/app/not-found.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function NotFound() {
+  return (
+    <div className="erro404">
+        <h1>Ocorreu um erro ao tentar acessar está pagina -  404</h1>
+        <p>Página não encontrada!</p>
+    </div> 
+  )
+}
