2d12c58626704a9dc90aba8052f644ae600752f5
esterdsilva <rm97798@fiap.com.br>
2023-11-10 16:39:17 -0300

    page uploud criado

---
+++ src/app/uploud/layout.jsx
@@ -0,0 +1,57 @@
+import ComponenteLogo from "../components/ComponenteLogo";
+import UploadFoto from "../images/UploadFoto.png";
+import ComponenteUpload from "../components/ComponenteUpload";
+import NavButtons from "../components/NavButtons";
+import "../styles/Upload.css";
+
+export default function Upload() {
+  return (
+    <main>
+      <div className="logo">
+        <ComponenteLogo />
+      </div>
+      <div className="colunmConfirmacao">
+      <div className="LinksNavUpload">
+        <NavButtons
+        anterior="cadastro/bike"
+        proximo="confirmacao"
+        />
+      </div>
+      <div className="titulo">
+        <h1>VISTORIA</h1>
+        <div className="instrucaoFoto">
+        <ul>
+          <li>
+            Escolha um local bem iluminado e com um fundo neutro para que a
+            bicicleta seja claramente visível.
+          </li>
+          <li>Evite áreas com muita sombra ou reflexos.</li>
+          <li>Tire as fotos de acordo com as posições indicadas.</li>
+          <li>Realize o upload das imagens no local indicado.</li>
+        </ul>
+        </div>
+      </div>
+      <div className="alinhamentoUpload">
+        <img src={UploadFoto} alt="Upload" className="uploadFoto" />
+      </div>
+      <div className="Upload-container">
+        <div className="fotoFrente">
+          <ComponenteUpload titulo="Foto Frontal"></ComponenteUpload>
+        </div>
+
+        <div className="fotoLateralEsquerda">
+          <ComponenteUpload titulo="Foto Lateral Esquerda"></ComponenteUpload>
+        </div>
+
+        <div className="fotoLateralDireita">
+          <ComponenteUpload titulo="Foto Lateral Direita"></ComponenteUpload>
+        </div>
+
+        <div className="fotoNumSerie">
+          <ComponenteUpload titulo="Foto Número Série"></ComponenteUpload>
+        </div>
+      </div>
+      </div>
+    </main>
+  );
+}
