91a775c5a2912aa25f47a621727f73202b1b5e72
oleoz1n <leopity0@gmail.com>
2023-11-10 21:38:51 -0300

    Refactor upload page and remove unused code

--- src/app/confirmacao/page.jsx
+++ src/app/confirmacao/page.jsx
@@ -1,32 +1,32 @@
-import ComponenteLogo from "../components/ComponenteLogo";
-import PagConfirmacao from "../images/PagConfirmacao.png";
-import "../confirmacao/confirmacao.css";
+// import ComponenteLogo from "../components/ComponenteLogo";
+// import PagConfirmacao from "../images/PagConfirmacao.png";
+// import "../confirmacao/confirmacao.css";
 
-export default function Confirmacao() {
-  return (
-    <main className="mainConfirmacao">
-      <div className="conteudoConfirmacao">
-        <div className="logo">
-          <ComponenteLogo />
-        </div>
-        <div className="conteudoConfirmacao">
-          <div className="centralizarImgConfirmacao">
-            <img
-              src={PagConfirmacao}
-              alt="Confirmacao"
-              className="imgPagConfirmacao"
-            />
-          </div>
+// export default function Confirmacao() {
+//   return (
+//     <main className="mainConfirmacao">
+//       <div className="conteudoConfirmacao">
+//         <div className="logo">
+//           <ComponenteLogo />
+//         </div>
+//         <div className="conteudoConfirmacao">
+//           <div className="centralizarImgConfirmacao">
+//             <img
+//               src={PagConfirmacao}
+//               alt="Confirmacao"
+//               className="imgPagConfirmacao"
+//             />
+//           </div>
 
-          <div className="tituloPagConfirmacao">
-            <h1>FOTOS ENVIADAS COM SUCESSO! </h1>
-            <h1>
-              AS IMAGENS ESTÃO SENDO ANALISADAS PELA NOSSA EQUIPE E EM BREVE
-              ENTRAREMOS EM CONTATO PELO E-MAIL CADASTRADO.
-            </h1>
-          </div>
-        </div>
-      </div>
-    </main>
-  );
-}
+//           <div className="tituloPagConfirmacao">
+//             <h1>FOTOS ENVIADAS COM SUCESSO! </h1>
+//             <h1>
+//               AS IMAGENS ESTÃO SENDO ANALISADAS PELA NOSSA EQUIPE E EM BREVE
+//               ENTRAREMOS EM CONTATO PELO E-MAIL CADASTRADO.
+//             </h1>
+//           </div>
+//         </div>
+//       </div>
+//     </main>
+//   );
+// }
--- src/app/upload/page.jsx
+++ src/app/upload/page.jsx
@@ -1,49 +1,42 @@
-import UploadFoto from "/public/UploadFoto.png";
 import ComponenteUpload from "@/components/ComponenteUploud";
 import NavButtons from "@/components/NavButtons/NavButtons";
+import styles from "./Upload.module.css";
 
 export default function Upload() {
   return (
-    <main>
-      <div className="colunmConfirmacao">
-        <div className="LinksNavUpload">
-          <NavButtons anterior="cadastro/bike" proximo="confirmacao" />
-        </div>
-        <div className="titulo">
-          <h1>VISTORIA</h1>
-          <div className="instrucaoFoto">
-            <ul>
-              <li>
-                Escolha um local bem iluminado e com um fundo neutro para que a
-                bicicleta seja claramente visível.
-              </li>
-              <li>Evite áreas com muita sombra ou reflexos.</li>
-              <li>Tire as fotos de acordo com as posições indicadas.</li>
-              <li>Realize o upload das imagens no local indicado.</li>
-            </ul>
-          </div>
-        </div>
-        <div className="alinhamentoUpload">
-          <img src={UploadFoto} alt="Upload" className="uploadFoto" />
-        </div>
-        <div className="Upload-container">
-          <div className="fotoFrente">
-            <ComponenteUpload titulo="Foto Frontal"></ComponenteUpload>
-          </div>
-
-          <div className="fotoLateralEsquerda">
-            <ComponenteUpload titulo="Foto Lateral Esquerda"></ComponenteUpload>
-          </div>
-
-          <div className="fotoLateralDireita">
-            <ComponenteUpload titulo="Foto Lateral Direita"></ComponenteUpload>
-          </div>
-
-          <div className="fotoNumSerie">
-            <ComponenteUpload titulo="Foto Número Série"></ComponenteUpload>
-          </div>
-        </div>
+    <main className={styles.uploadMain}>
+       <NavButtons anterior="cadastro/bike" proximo="confirmacao" />
+  <div className={styles.colunmConfirmacao}>
+    <div className={styles.titulo}>
+      <h1>VISTORIA</h1>
       </div>
-    </main>
+      <div className={styles.instrucaoFoto}>
+        <ul>
+          <li>
+            Escolha um local bem iluminado e com um fundo neutro para que a
+            bicicleta seja claramente visível.
+          </li>
+          <li>Evite áreas com muita sombra ou reflexos.</li>
+          <li>Tire as fotos de acordo com as posições indicadas.</li>
+          <li>Realize o upload das imagens no local indicado.</li>
+        </ul>
+      </div>
+    <div className={styles.UploadContainer}>
+      <div className={styles.fotoFrente}>
+        <ComponenteUpload titulo="Foto Frontal"></ComponenteUpload>
+      </div>
+      <div className={styles.fotoLateralEsquerda}>
+        <ComponenteUpload titulo="Foto Lateral Esquerda"></ComponenteUpload>
+      </div>
+      <div className={styles.fotoLateralDireita}>
+        <ComponenteUpload titulo="Foto Lateral Direita"></ComponenteUpload>
+      </div>
+      <div className={styles.fotoNumSerie}>
+        <ComponenteUpload titulo="Foto Número Série"></ComponenteUpload>
+      </div>
+    </div>
+  </div>
+</main>
+
   );
 }
